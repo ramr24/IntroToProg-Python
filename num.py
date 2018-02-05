aff078cfa464df001575cc070899342b8d9b1fef
Ramkumar Rajanbabu <33403970+ramr24@users.noreply.github.com>
2018-02-04 19:26:47 -0800

    Assignment #2

---
+++ num.py
@@ -0,0 +1,21 @@
+#Assignment_02
+'''
+Create a new program that asks the user to input 2 numbers 
+then prints out the sum, difference, product, and quotient.
+'''
+
+#Input 2 numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#Functions
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+#Print statements
+print("Sum:", add)
+print("Difference:", sub)
+print("Product:", mul)
+print("Quotient:", div)
